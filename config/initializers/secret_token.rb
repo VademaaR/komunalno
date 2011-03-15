cf459928a4b744839c743b0c3f16e3092155c150
MiKe <mike.cheverda@gmail.com>
2011-03-15 21:24:28 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Komunalno::Application.config.secret_token = '8e7937ebed26e94641d0ef34235c6e7b9586ec3a7825fd678c5040df190a7c7231c1519609ad4de444f3a7be3ed18fe101f6cfcca399050ef7e66c4bca0428d6'
